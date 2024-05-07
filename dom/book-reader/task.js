e544c1aeb4083c4352f04a2aa5f488d7f362d10b
admin mary <atanova.mary@gmail.com>
2024-05-07 22:23:28 +0400

    adding tasks

---
+++ dom/book-reader/task.js
@@ -0,0 +1,26 @@
+const fontSizeButtons = Array.from(document.querySelectorAll('.font-size'));
+
+fontSizeButtons.forEach((btn) => {
+    btn.addEventListener('click', switchSize);
+});
+
+function switchSize(event) {
+    event.preventDefault();
+    fontSizeButtons.forEach((btn) => {
+        if (btn.classList.contains("font-size_active")) {
+            btn.classList.remove("font-size_active");
+            book.classList.remove("book_fs-small");
+            book.classList.remove("book_fs-big");
+          }
+        });
+
+        if (this.classList.contains("font-size_small")) {
+            this.classList.add("font-size_active");
+            book.classList.add("book_fs-small");
+        } else if (this.classList.contains("font-size_big")) {
+            this.classList.add("font-size_active");
+            book.classList.add("book_fs-big");
+        } else if (book.classList != "book_fs-big" && book.classList != "book_fs-small") {
+            this.classList.add("font-size_active");
+        }
+}
